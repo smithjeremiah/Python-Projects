6836b0d1eba07a933d233535d0574fe6b0140b00
smithjeremiah <jeremiahjsmith21@gmail.com>
2019-03-12 12:19:17 -0700

    Initial commit

---
+++ yahoofinancestock.py
@@ -0,0 +1,88 @@
+import urllib.parse, urllib.error, urllib.request
+from bs4 import BeautifulSoup
+import sqlite3
+import time
+import datetime
+
+
+def stockcheck():
+    fh = urllib.request.urlopen('https://finance.yahoo.com/most-active')
+    ans = fh.read()
+    soup = BeautifulSoup(ans,'html.parser')
+
+
+    lst = list(soup.strings)
+
+#stores stock prices in stocklst
+    stocklst = []
+    face = False#Used to omit text that isn't stock prices
+    for i in lst:
+        if i == '52 Week Range':
+            face = True
+            continue
+        if face == True:
+            stocklst.append(i)
+        if i[0:2] == 'if' :
+            face = False
+
+    stocklst.pop()# Removes unecessary 'if line'
+
+
+    tuple_lst = []
+
+    while len(stocklst) > 0:
+        tuple_lst.append((stocklst[0],stocklst[1],stocklst[2],stocklst[3],stocklst[4],stocklst[5],stocklst[6],stocklst[7],stocklst[8]))
+        del(stocklst[:9])
+
+    for i in tuple_lst:
+        print(i)
+
+# put the info from tuple_lst into a database via sqlite
+    conn = sqlite3.connect('yahoostock.db')
+    cur = conn.cursor()
+
+    cur.executescript('''
+        DROP TABLE IF EXISTS Stocks;
+
+        CREATE TABLE Stocks(
+        id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+        name TEXT UNIQUE,
+        price TEXT ,
+        change TEXT ,
+        percent_change TEXT ,
+        volume TEXT ,
+        avg_vol_3_month TEXT ,
+        market_cap TEXT ,
+        pe_ratio TEXT
+        );
+
+        ''')
+
+    for item in tuple_lst:
+        cur.execute('''INSERT INTO Stocks
+        (name, price, change, percent_change, volume, avg_vol_3_month, market_cap, pe_ratio)
+        VALUES(?,?,?,?,?,?,?,?)''',
+        (item[0], item[1], item[2], item[3], item[4], item[5], item[6], item[7]))
+
+    conn.commit()
+
+stockcheck()
+print(datetime.date.today() + datetime.timedelta(days = 1))
+
+tday = datetime.date.today()
+tmrrow = datetime.date.today() + datetime.timedelta(days = 1)
+print('start')
+
+#makes it run everyday
+
+while True:
+
+    time.sleep(30000)
+    tday = datetime.date.today()
+    if tday == tmrrow:
+        stockcheck()
+        tmrrow = datetime.date.today() + datetime.timedelta(days = 1)
+        continue
+    print(tday)
+    print(tmrrow)
+    print('here')
